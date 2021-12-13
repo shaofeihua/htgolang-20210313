18fa7fd339680ea6c6488b95d4dae567eaa25261
卢思波(lusibo) <lusibo@gome.com.cn>
2021-12-13 19:10:58 +0800

    DellPC At 2021.12.13

--- course/day11-20210620/codes/cmdb/config/conf.go
+++ course/day11-20210620/codes/cmdb/config/conf.go
@@ -1,6 +1,6 @@
 package config
 
 var DbType = "mysql"
-var DbDSN = "golang:golang@2021@tcp(10.0.0.2:3306)/cmdb?charset=utf8mb4&parseTime=true"
-
+//var DbDSN = "golang:golang@2021@tcp(10.0.0.2:3306)/cmdb?charset=utf8mb4&parseTime=true"
+var DbDSN = "golang:9tUz2LK8wymRLz3@tcp(10.106.37.131:3306)/cmdb?charset=utf8mb4&parseTime=true"
 var ServerAddr = ":9999"
--- course/day11-20210620/codes/cmdb/go.mod
+++ course/day11-20210620/codes/cmdb/go.mod
@@ -1,9 +1,9 @@
 module cmdb
 
-go 1.16
+go 1.17
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0
-	github.com/google/uuid v1.2.0
-	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
+	github.com/google/uuid v1.3.0
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871
 )
